b0a89b7b880531f3261f367ee47dda1f450232e5
Preston Foote <prestonfoote#@mail.weber.edu>
2020-04-02 13:48:48 -0600

    all problems buts 18

--- array.py
+++ array.py
@@ -4,17 +4,38 @@ cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC',]
 
 cars.append('Buick')
 
+print (cars)
+
 x = len(cars)
 
-z = cars[4]
-
-cars.insert(3, 'Toyota')
-
-cars.pop(5)
-
 print (x)
+
+print (cars)
+
+z = cars[4]
 
 print (z)
 
 print (cars)
 
+cars.insert(3, 'Toyota')
+
+print (cars)
+
+cars.pop(5)
+
+print (cars)
+
+cars.sort()
+
+print (cars)
+
+cars.sort(reverse=True)
+
+print (cars)
+
+my_array_length = (x)
+
+array_string = 'The length of my array is '
+
+print (array_string + my_array_length)
